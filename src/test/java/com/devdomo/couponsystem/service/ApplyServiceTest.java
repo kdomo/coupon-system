a15008db20360cfd5d37abdcc1cdb9c84da07275
kdomo <dongho5088@naver.com>
2024-10-21 17:05:15 +0900

    test: 1회 응모 테스트

---
+++ src/test/java/com/devdomo/couponsystem/service/ApplyServiceTest.java
@@ -0,0 +1,32 @@
+package com.devdomo.couponsystem.service;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.devdomo.couponsystem.repository.CouponRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplyServiceTest {
+
+    @Autowired
+    private ApplyService applyService;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Test
+    void 한번만_응모() {
+        applyService.apply(1L);
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(1);
+    }
+}
