dd1b26bf379c9bc80b37e0ac1ada763ae585f4d8
kdomo <dongho5088@naver.com>
2024-10-24 15:47:43 +0900

    feat: 한명 당 한개의 쿠폰만 발급하도록 제한

--- src/main/java/com/devdomo/couponsystem/service/ApplyService.java
+++ src/main/java/com/devdomo/couponsystem/service/ApplyService.java
@@ -2,6 +2,7 @@ package com.devdomo.couponsystem.service;
 
 import com.devdomo.couponsystem.domain.Coupon;
 import com.devdomo.couponsystem.producer.CouponCreateProducer;
+import com.devdomo.couponsystem.repository.AppliedUserRepository;
 import com.devdomo.couponsystem.repository.CouponCountRepository;
 import com.devdomo.couponsystem.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -11,14 +12,22 @@ public class ApplyService {
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
     private final CouponCreateProducer couponCreateProducer;
+    private final AppliedUserRepository appliedUserRepository;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer, AppliedUserRepository appliedUserRepository) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
     public void apply(Long userId) {
+        Long apply = appliedUserRepository.add(userId);
+
+        if (apply != 1) {
+            return;
+        }
+
         Long count = couponCountRepository.increment();
 
         if (count > 100) {
