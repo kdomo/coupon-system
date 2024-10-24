665d9641c2b9ade9a2032b0ac857dfffae146ba2
kdomo <dongho5088@naver.com>
2024-10-24 14:43:33 +0900

    chore: consumer 모듈 생성

---
+++ consumer/src/test/java/com/github/kdomo/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kdomo.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
